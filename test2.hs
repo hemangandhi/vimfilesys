7859e4b39d2cd546b2a36f1af38e7bb8bcdecdf3
Heman Gandhi <hemangandhi@gmail.com>
2017-05-20 17:51:39 -0400

    no visible output

--- test2.hs
+++ test2.hs
@@ -1,27 +1,26 @@
 import UI.NCurses
 import System.Directory
 
-main = do
-    runCurses $ do
-        setEcho False
-        w <- defaultWindow
-        handleEvents w jkHandler 1 (- 1)
+main = runCurses $ do
+           setEcho False
+           w <- defaultWindow
+           drawLs 1 w
+           -- render
+           handleEvents w jkHandler 1 (- 1)
 
-drawLs :: Integer -> Window -> Curses (IO (Curses ()))
-drawLs row w = return $ lsCwdUpdate >>= \cwd_ls -> return $ updateWindow w
-                                                          $ foldr (>>) clear
-                                                          $ map render_row
-                                                          $ zip cwd_ls [0..]
-             where render_row (path, idx) = do setAttribute AttributeUnderline (idx == row)
-                                               moveCursor idx 20
-                                               drawString path
-                                               moveCursor 0 0
+drawLs :: Integer -> Window -> Curses ()
+drawLs row w = (return $ lsCwdUpdate >>= \cwd_ls -> return ((mapM render_row $ zip cwd_ls [0..]) >> render)) >> return ()
+             where render_row (path, idx) = updateWindow w $ do setAttribute AttributeUnderline (idx == row)
+                                                                moveCursor idx 20
+                                                                drawString path
+                                                                moveCursor 0 0
                    lsCwdUpdate = (getCurrentDirectory >>= listDirectory)
 
 jkHandler :: Event -> Integer -> Window -> Curses Integer
 jkHandler (EventCharacter 'j') i w = drawLs (i + 1) w >> return (i + 1)
 jkHandler (EventCharacter 'k') i w = drawLs (i - 1) w >> return (i - 1)
 jkHandler (EventCharacter 'q') i w = drawLs i w >> return (-1)
+jkHandler _                    i w = return i
 
 handleEvents :: (Eq a) => Window -> (Event -> a -> Window -> Curses a) -> a -> a -> Curses ()
 handleEvents w f start stop = loop start
@@ -31,6 +30,7 @@ handleEvents w f start stop = loop start
                                         Nothing  -> loop st
                                         Just ev' -> let c_next = f ev' st w in do
                                             next <- c_next
+                                            -- render
                                             if next == stop
-                                            then return ()
-                                            else loop next
+                                                then return ()
+                                                else loop next
