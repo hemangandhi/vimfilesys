c022fe77dbc972d2d64934adafa74bf04dd1c50c
Heman Gandhi <hemangandhi@gmail.com>
2017-05-20 22:08:02 -0400

    basic movement and opening

--- test2.hs
+++ test2.hs
@@ -1,5 +1,6 @@
 import UI.NCurses
 import System.Directory
+import System.Process
 import Control.Monad.IO.Class
 
 main = runCurses $ do
@@ -10,17 +11,32 @@ main = runCurses $ do
 
 drawLs :: Integer -> Window -> Curses()
 drawLs i w = do ls <- liftIO (getCurrentDirectory >>= listDirectory)
+                updateWindow w clear
                 mapM (\(dir, idx) -> updateWindow w $ do
                                      setAttribute AttributeUnderline (idx == i)
                                      moveCursor idx 20
                                      drawString dir
-                                     moveCursor 0 0)
+                                     moveCursor i 0)
                      $ zip ls [0..]
                 render
+
+setWd :: FilePath -> Curses ()
+setWd path = liftIO $ do abs_pth <- makeAbsolute path
+                         isDir <- doesDirectoryExist abs_pth
+                         if isDir then setCurrentDirectory abs_pth
+                                  else (spawnCommand ("urxvt -e zsh -c \"vim "
+                                                      ++ abs_pth ++ "\"")
+                                        >> return ())
+
+getCurrPath :: Integer -> Curses FilePath
+getCurrPath i = liftIO $ getCurrentDirectory >>= listDirectory
+                                             >>= return . (!! (fromInteger i))
 
 jkHandler :: Event -> Integer -> Window -> Curses Integer
 jkHandler (EventCharacter 'j') i w = drawLs (i + 1) w >> return (i + 1)
 jkHandler (EventCharacter 'k') i w = drawLs (i - 1) w >> return (i - 1)
+jkHandler (EventCharacter 'l') i w = getCurrPath i >>= setWd >> drawLs 0 w >> return 0
+jkHandler (EventCharacter 'h') i w = setWd ".." >> drawLs 0 w >> return 0
 jkHandler (EventCharacter 'q') i w = drawLs i w >> return (-1)
 jkHandler _                    i w = return i
 
